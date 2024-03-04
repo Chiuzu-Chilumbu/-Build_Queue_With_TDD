4af8e1f711228655b8f6d381a3f596cd2d3489f4
Chiuzu-Chilumbu <Chiuzu-Chilumbu@users.noreply.github.com>
2024-03-04 21:33:46 +0000

    deploy: 3513c0f178d540208c1e29c4d2df4cf4398faa1f

--- queue/queue_adt.py
+++ queue/queue_adt.py
@@ -1,14 +1,33 @@
 """implementation of the queue ADT using a Python deque"""
 from collections import deque
 
+
 class Queue:
-    def __init__(self):
-        self.queue = deque()
+    def __init__(self, capacity):
+        #  ensure the capacity is of integer type
+        if not isinstance(capacity, int):
+            raise TypeError('The queue capacity must be of type int')
+        else:
+            self.queue = deque()
+            self.capacity = capacity
 
     def enqueue(self, item):
-        self.queue.append(item)
-        return self.queue
+        """
+        Add data to the end of the queue.
+        Params -> Data: any Python data type, e.g., int, str, etc.
+        """
+        if len(self.queue) == self.capacity:
+            raise Exception('Queue is Full')
+        else:
+            self.queue.append(item)
+            return self.queue
+
+    def dequeue(self):
+        """Remove data from the queue"""
+        if len(self.queue) == 0:
+            raise Exception('Queue is empty')
+        else:
+            return self.queue.popleft()
 
     def size(self):
         return len(self.queue)
-
